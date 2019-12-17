535ef58c64dfa6833e4f4741ebedfc553a13ec3d
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-17 20:04:41 +0800

    Add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const bookingRouter = require("./routes/bookings.routes");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: [
+    "https://cinema-ticket-booking.netlify.com",
+    "http://localhost:3000",
+    "http://localhost:3005"
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true
+};
 
 if (app.get("env") !== "test") {
   require("./utils/db");
 }
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/bookings", bookingRouter);
