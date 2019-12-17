42ea0c6e0724b1179c75575262406c241fc3da38
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-17 17:52:21 +0800

    Change port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const env = app.get("env");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} in ${env} mode`);
