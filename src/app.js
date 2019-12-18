1b4f4bb460c5daed7bd7d1906f9def0eac376105
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-18 21:15:27 +0800

    Update cors origin to include production front end

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "https://cinema-ticket-booking.netlify.com",
+    "https://cinema-ticket-booking.herokuapp.com/",
     "http://localhost:3000",
     "http://localhost:3005"
   ],
